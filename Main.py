f33c60eb6cf6420e4d60f6c30e89f28d237eab1e
sashi1994 <53015870+sashi1994@users.noreply.github.com>
2025-04-05 16:36:42 +0200

    version 2

---
+++ Main.py
@@ -0,0 +1,13 @@
+from SRC.Utils.UnitConverter import UnitConverter
+import pandas as pd 
+
+
+df = pd.DataFrame({
+    "length_mm": [1000, 1500, 2000],
+    "width_mm": [500, 750, 1000]
+})
+
+df["length_mm"] = UnitConverter.convert_vector(df["length_mm"] , "gravity", "m/s^2")
+df= UnitConverter.convert_value(1, "gravity", "m/s^2")
+
+print(df)
---
+++ SRC/Utils/UnitConverter.py
@@ -0,0 +1,62 @@
+import pint
+import pandas as pd
+
+convert = pint.UnitRegistry().Quantity
+
+
+class UnitConverter:
+
+    @staticmethod
+    def convert_vector(vector, from_unit:'str', to_unit:'str') -> pd:
+        """
+        Convert a list, tuple, or pandas Series of numbers from one unit to another.
+        Returns a list of converted magnitudes (floats).
+        Raises:
+            TypeError: if input is not a valid sequence
+            ValueError: if units are not valid
+        """
+
+        # Check if vector is list-like
+        if not isinstance(vector, (list, tuple, pd.Series)):
+            raise TypeError("Input must be a list, tuple, or pandas Series.")
+
+        # Check if units are valid using Pint
+        try:
+            pint.UnitRegistry().Unit(from_unit)
+            pint.UnitRegistry().Unit(to_unit)
+
+        except convert.UndefinedUnitError as e:
+            raise ValueError(f"Invalid unit: {e}")
+
+        # Convert
+
+        return [convert(value, from_unit).to(to_unit).magnitude for value in vector]
+    
+
+
+    @staticmethod
+    def convert_value(var, from_unit:'str', to_unit:'str') -> pd:
+        """
+        Convert a list, tuple, or pandas Series of numbers from one unit to another.
+        Returns a list of converted magnitudes (floats).
+        Raises:
+            TypeError: if input is not a valid sequence
+            ValueError: if units are not valid
+        """
+
+        # Check if vector is list-like
+        if not isinstance(var, (int, float)):
+            raise TypeError("Input must be a float and integer.")
+
+        # Check if units are valid using Pint
+        try:
+            pint.UnitRegistry().Unit(from_unit)
+            pint.UnitRegistry().Unit(to_unit)
+
+        except pint.errors.UndefinedUnitError as e:
+            raise ValueError(f"Invalid unit: {e}")
+
+        # Convert each value in the vector or scalar
+        return convert(var, from_unit).to(to_unit).magnitude
+    
+    
